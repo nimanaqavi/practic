5384a086e1584b5309a827edf0fe78d2106990c9
Nima naqavi <nima2005naqavi@gmail.com>
2025-02-25 00:24:34 +0330

    update main1.py

---
+++ src/main/main1.py
@@ -0,0 +1,33 @@
+# coment
+'''
+coment1
+coment2
+'''
+# print("hi\nhi")
+
+x = 2
+y = 1.2
+z = "ali"
+t = True
+f = False
+n = None
+
+m = "Hello " + z
+print(m)
+my_list = [1,2,3,4,5,6,7,8]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[1:4]) # until last-1
+print(my_list[1:]) # until last
+my_list.append('Nima')
+print(my_list)
+
+
+my_list.insert(0, "vahid")
+print(my_list)
+
+my_list.remove('vahid')
+print(my_list)
+
+print(len(my_list))
